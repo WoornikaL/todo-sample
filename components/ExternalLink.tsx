38ec26189d4758eed8db9f04e005fbf7feef74aa
Woornika Liyanage <woornika@ideabits.se>
2024-07-31 13:58:56 +0530

    Commit generated on 31/07/2024, 13:58:50

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -19,7 +19,7 @@ export function ExternalLink({ href, ...rest }: Props) {
           await openBrowserAsync(href);
           const xyz = 1
           if(xyz === 1) {
-            console.log('xyz is 1')
+            console.log('xyz value is 1')
           }
         }
       }}
