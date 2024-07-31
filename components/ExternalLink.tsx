6b05b5817ef063c4fde412147c51d59396b89fe8
Woornika Liyanage <woornika@ideabits.se>
2024-07-31 15:51:41 +0530

    commit2

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -12,6 +12,7 @@ export function ExternalLink({ href, ...rest }: Props) {
       {...rest}
       href={href}
       onPress={async (event) => {
+        const xyz = 1
         if (Platform.OS !== 'web') {       // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
           console.log(event.target)
@@ -22,10 +23,16 @@ export function ExternalLink({ href, ...rest }: Props) {
           if(xyz === 1) {
             console.log('hjhjhjh value 1')
           }
+          if(xyz === 2) {
+            console.log('hjhjhjh value 2')
+          }
+          if( Platform.OS === "android") {
+            console.log("android platform")
+          }
+       
        
           // Open the link in an in-app browser. 
           await openBrowserAsync(href);
-         
          
         }
       }}
