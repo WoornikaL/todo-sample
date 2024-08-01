4c71c7b3b9144bba020bf2592ed8f120040f4188
woornika@ideabits.se <woornika@ideabits.se>
2024-08-01 22:45:49 +0530

    Update comment in `Collapsible.tsx` render method
    - Refined comment from "render method comment new edited 2" to "render method comment new edited 22"

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -10,7 +10,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
   const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? 'light';
 
-  // render method comment new edited 2
+  // render method comment new edited 22
   return (
     <ThemedView>
       <TouchableOpacity
