02b314679c38810c21df99eff25e426cc8f5eb17
woornika@ideabits.se <woornika@ideabits.se>
2024-08-01 17:24:24 +0530

    Commit generated on 01/08/2024, 17:24:23

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -36,6 +36,7 @@ export function ExternalLink({ href, ...rest }: Props) {
           if(Platform.isTesting) {
             console.log("ios 112 1")
           }
+          // Added
 
 
           //changed this line
