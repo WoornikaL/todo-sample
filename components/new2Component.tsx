c9ac632e3b8ac814ae13cc1d6478cd39769aedaa
woornika@ideabits.se <woornika@ideabits.se>
2024-08-02 16:47:27 +0530

    **Update `new2Component.tsx` to fix typo in the comment**
    Modified a comment to correct a minor typo for better readability.

--- components/new2Component.tsx
+++ components/new2Component.tsx
@@ -15,7 +15,7 @@ export function NewComponent() {
 
   rotationAnimation.value = withRepeat(
     withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation 4 timeshghgkkkhhhhhhhh
+    4 // Run the animation 4 timeshghgkkkhhhhhhhhgg
   );
 //
   const animatedStyle = useAnimatedStyle(() => ({
