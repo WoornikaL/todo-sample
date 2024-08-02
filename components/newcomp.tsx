88060eaa6bc546081dc933cef3a2e1766c1ffe22
Woornika Liyanage <woornika@ideabits.se>
2024-08-02 18:48:07 +0530

    commit on another pro

--- components/newcomp.tsx
+++ components/newcomp.tsx
@@ -18,19 +18,12 @@ export function NewThemedText({
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 //
   return (
-<<<<<<< HEAD
-    <View>
-<Text> AAAAA </Text>
-    </View>
-
-=======
    <View> 
-    <Text>ffffffff</Text>
-    <Text> bbbbb....</Text>
+    <Text>ffffffffhghghghg</Text>
+    <Text> bbbbbghggg....</Text>
    </View>>
       {...rest}
     />
->>>>>>> 944563ce73e1d826f9352e563be5f07315d989fe
   );
 }
 
