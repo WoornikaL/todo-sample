e16458044879f021b4c596b5fe256a8d4ec23955
woornika@ideabits.se <woornika@ideabits.se>
2024-08-02 09:04:40 +0530

    Update comment in Collapsible.tsx for style information.

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -29,7 +29,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
   );
 }
 
-//styles added more egggggyuyuy
+//styles added more egggggyuyuyjhjhjh
 const styles = StyleSheet.create({
   heading: {
     flexDirection: 'row',
