0c5f128cb45cf119ec1bbe89fda971a22349cdd0
woornika@ideabits.se <woornika@ideabits.se>
2024-08-02 09:10:25 +0530

    ``` Fix typo in comment within Collapsible.tsx ```

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -29,7 +29,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
   );
 }
 
-//styles added more egggggyuyuyjhjhjh
+//styles added more egggggyuyuyjhjhjhhjhjhj
 const styles = StyleSheet.create({
   heading: {
     flexDirection: 'row',
