8cdf16419c20f3c6202741c1498ea16d35514601
woornika@ideabits.se <woornika@ideabits.se>
2024-08-02 15:08:34 +0530

    Update comment in new2Component.tsx to fix grammatical mistake

--- components/new2Component.tsx
+++ components/new2Component.tsx
@@ -15,7 +15,7 @@ export function NewComponent() {
 
   rotationAnimation.value = withRepeat(
     withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation 4 timeshghgkkk
+    4 // Run the animation 4 timeshghgkkkhhh
   );
 //
   const animatedStyle = useAnimatedStyle(() => ({
