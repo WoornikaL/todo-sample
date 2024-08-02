81a0ba4b77eb7c482c559c2d731fa721cefb7257
woornika@ideabits.se <woornika@ideabits.se>
2024-08-02 08:43:22 +0530

    Update comment styles in Collapsible.tsx
    - Changed comment text to improve readability and clarity.

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -29,7 +29,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
   );
 }
 
-//styles added more 2
+//styles added more e
 const styles = StyleSheet.create({
   heading: {
     flexDirection: 'row',
