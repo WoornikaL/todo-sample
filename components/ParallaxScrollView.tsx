f9b4076aad72652c76ad4eaa3cd90537ba70ae46
woornika@ideabits.se <woornika@ideabits.se>
2024-08-03 11:52:22 +0530

    Checkin conlicts resolving editor

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -11,7 +11,7 @@ import { ThemedView } from '@/components/ThemedView';
 
 const HEADER_HEIGHT = 250;
 //
-// Add another
+// Add another comment
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
   headerBackgroundColor: { dark: string; light: string };
