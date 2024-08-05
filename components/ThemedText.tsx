54211c9c0590e013823fc7817dd6912945e0ae69
woornika@ideabits.se <woornika@ideabits.se>
2024-08-05 14:16:05 +0530

    Resolved Conflicts.

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,10 +22,10 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.link : undefined,
-        type === 'subtitle' ? styles.default : undefined,
-        type === 'title' ? styles.defaultSemiBold : undefined,
-        type === 'defaultSemiBold' ? styles.title : undefined,
+        type === 'default' ? styles.default : undefined,
+        type === 'subtitle' ? styles.title : undefined,
+        type === 'title' ? styles.link : undefined,
+        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
 
         style,
       ]}
