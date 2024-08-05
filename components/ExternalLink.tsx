164f292be4eccfa3f6e1b596db9f8d957c965acf
woornika@ideabits.se <woornika@ideabits.se>
2024-08-05 23:50:47 +0530

    hjhjhjhjhj

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -36,7 +36,7 @@ export function ExternalLink({ href, ...rest }: Props) {
           if(Platform.isTesting) {
             console.log("112 1")
           }
-          // Added new comment gggg
+          // Added new comment gggg gggg
 
 
           //changed this line
