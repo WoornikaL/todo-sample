ba70de3fae68fde28dd2e1713dd0188458735790
woornika@ideabits.se <woornika@ideabits.se>
2024-08-06 00:33:42 +0530

    hhhhh

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,10 +22,10 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.title : undefined,
-        type === 'subtitle' ? styles.defaultSemiBold : undefined,
-        type === 'title' ? styles.link : undefined,
-        type === 'defaultSemiBold' ? styles.default : undefined,
+        type === 'default' ? styles.default : undefined,
+        type === 'subtitle' ? styles.link : undefined,
+        type === 'title' ? styles.defaultSemiBold : undefined,
+        type === 'defaultSemiBold' ? styles.title : undefined,
 
         style,
       ]}
