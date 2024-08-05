224fd80ddc499ee348546f10f8971caca8c56ad0
woornika@ideabits.se <woornika@ideabits.se>
2024-08-05 22:02:45 +0530

    fsdffdsfs

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -15,7 +15,7 @@ export function HelloWave() {
 
   rotationAnimation.value = withRepeat(
     withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation6 4 7fffff changed added added
+    4 // Run the animation6 4 7fffff changed added added kkkk
   );
 
   const animatedStyle = useAnimatedStyle(() => ({
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,9 +22,9 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.title : undefined,
-        type === 'subtitle' ? styles.link : undefined,
-        type === 'title' ? styles.defaultSemiBold : undefined,
+        type === 'default' ? styles.defaultSemiBold : undefined,
+        type === 'subtitle' ? styles.title : undefined,
+        type === 'title' ? styles.link : undefined,
         type === 'defaultSemiBold' ? styles.default : undefined,
 
         style,
