0dc53e42175c3d9891aa19795e42b4cdce0a472a
woornika@ideabits.se <woornika@ideabits.se>
2024-08-05 13:57:20 +0530

    Update `ParallaxScrollView.tsx`: Add a new comment for change tracking.Update ThemedText.tsx to modify text styling conditions
    - Changed 'default' type style from `styles.default` to `styles.link`
    - Changed 'subtitle' type style from `styles.title` to `styles.default`
    - Changed 'defaultSemiBold' type style from `styles.link` to `styles.title`

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -10,7 +10,7 @@ import Animated, {
 import { ThemedView } from '@/components/ThemedView';
 
 const HEADER_HEIGHT = 250;
- // Adding New comment to check changes new changes adding comment added again
+ // Adding New comment to check changes new changes adding comment added again added new
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
   headerBackgroundColor: { dark: string; light: string };
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,10 +22,10 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'subtitle' ? styles.title : undefined,
+        type === 'default' ? styles.link : undefined,
+        type === 'subtitle' ? styles.default : undefined,
         type === 'title' ? styles.defaultSemiBold : undefined,
-        type === 'defaultSemiBold' ? styles.link : undefined,
+        type === 'defaultSemiBold' ? styles.title : undefined,
 
         style,
       ]}
