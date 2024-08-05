73546614993673ddce384b6516047073b0c6bc12
woornika@ideabits.se <woornika@ideabits.se>
2024-08-06 01:40:19 +0530

    fgfgfgfgf

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,7 +1,7 @@
 import { View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
-// added comment added
+// added comment added yyyy
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
