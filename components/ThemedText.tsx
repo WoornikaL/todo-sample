6765d716971ec0330c68189853a18312cdabb384
Woornika Liyanage <woornika@ideabits.se>
2024-08-06 17:17:41 +0530

    making conflict

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,10 +22,10 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.link : undefined,
-        type === 'subtitle' ? styles.title : undefined,
+        type === 'default' ? styles.default : undefined,
+        type === 'subtitle' ? styles.link : undefined,
         type === 'title' ? styles.default : undefined,
-        type === 'defaultSemiBold' ? styles.default : undefined,
+        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
 
         style,
       ]}
