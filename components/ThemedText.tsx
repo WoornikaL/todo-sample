c936953bec6c08cdc6591e64aef45d5d31c47ab1
Woornika Liyanage <woornika@ideabits.se>
2024-08-06 10:08:09 +0530

    dddd

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -22,9 +22,9 @@ export function ThemedText({
     <Text
       style={[
         { color },
-        type === 'default' ? styles.link : undefined,
-        type === 'subtitle' ? styles.defaultSemiBold : undefined,
-        type === 'title' ? styles.title : undefined,
+        type === 'default' ? styles.defaultSemiBold : undefined,
+        type === 'subtitle' ? styles.title : undefined,
+        type === 'title' ? styles.link : undefined,
         type === 'defaultSemiBold' ? styles.default : undefined,
 
         style,
