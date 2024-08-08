85cc4bb207c8ab5ed85c7b42bbfb63730f77a068
woornika@ideabits.se <woornika@ideabits.se>
2024-08-08 21:08:40 +0530

    vvvv

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -24,7 +24,7 @@ export default function ParallaxScrollView({
   const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
-//comment added edited edited abc
+//comment added edited edited abc bbbb aa
   const headerAnimatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
