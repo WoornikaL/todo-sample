995f6205dbad008914bfd8c964b49a34900ed3bb
woornika@ideabits.se <woornika@ideabits.se>
2024-08-13 13:09:50 +0530

    bbbbb

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -12,7 +12,7 @@ export function ExternalLink({ href, ...rest }: Props) {
       {...rest}
       href={href}
       onPress={async (event) => {
-        // Added new comment hh
+        // Added new comment hh hhhhh
         const xyz = 1
         if (Platform.OS !== 'web') {       // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -15,7 +15,7 @@ export function HelloWave() {
 
   rotationAnimation.value = withRepeat(
     withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-    4 // Run the animation6 ggggggg
+    4 // Run the animation
   );
 
   const animatedStyle = useAnimatedStyle(() => ({
