b83b3316c9f93f8181e605e2f2013feca9557886
Woornika Liyanage <woornika@ideabits.se>
2024-08-19 23:09:01 +0530

    abc my commit

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,14 @@
 import { View, type ViewProps } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
-// added comment added yyyy added
+// added comment added yyyy added aaa
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
 };
 
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColorr\ }, 'background');
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
